31c6a3d0afa7ec5e2578114c4cb3020eefec2d96
narendra-patel-np <narendrapatel.np70@gmail.com>
2023-12-18 18:40:51 +0530

    id

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const { connection } = require("./db");
-const {userRouter}=require("./routes/user.routes");
-const {oppoRouter} = require("./routes/oppo.routes");
+const { userRouter } = require("./routes/user.routes");
+const { oppoRouter } = require("./routes/oppo.routes");
 const cors = require("cors");
-require("dotenv").config()
+require("dotenv").config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/users",userRouter);
-app.use("/volunteer",oppoRouter);
+app.use("/users", userRouter);
+app.use("/volunteer", oppoRouter);
 
-app.listen(process.env.PORT, async () => {
+app.listen(4500, async () => {
   try {
     await connection;
-    console.log('Connect to the MogoDB');
+    console.log("Connect to the MogoDB");
   } catch (error) {
     console.log(error);
   }
-  console.log(`Connected to DB and Server is running at ${process.env.PORT}`);
+  console.log(`Connected to DB and Server is running at 4500`);
 });
 ///
